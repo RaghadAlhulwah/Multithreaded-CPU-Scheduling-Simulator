38296396d6ef974cb1caf6e7b25cf237fb98eb84
lamiis-1 <444201135@student.ksu.edu.sa>
2026-05-08 00:32:11 +0300

    Update GanttEntry.java

--- GanttEntry.java
+++ GanttEntry.java
@@ -1,43 +1,32 @@
 package com.mycompany.operatingsystemproject;
+ 
 
 public class GanttEntry {
-
+ 
     private int processId;
     private int startTime;
     private int endTime;
     private int startBurst;
     private int stopBurst;
-
-    public GanttEntry(int processId,
-                      int startTime,
-                      int endTime,
-                      int startBurst,
-                      int stopBurst) {
-
-        this.processId = processId;
-        this.startTime = startTime;
-        this.endTime = endTime;
+ 
+    // Constructor
+    
+ 
+   
+    public GanttEntry(int processId, int startTime, int endTime,
+                      int startBurst, int stopBurst) {
+        this.processId  = processId;
+        this.startTime  = startTime;
+        this.endTime    = endTime;
         this.startBurst = startBurst;
-        this.stopBurst = stopBurst;
+        this.stopBurst  = stopBurst;
     }
-
-    public int getProcessId() {
-        return processId;
-    }
-
-    public int getStartTime() {
-        return startTime;
-    }
-
-    public int getEndTime() {
-        return endTime;
-    }
-
-    public int getStartBurst() {
-        return startBurst;
-    }
-
-    public int getStopBurst() {
-        return stopBurst;
-    }
+ 
+    // Getters
+ 
+    public int getProcessId()  { return processId; }
+    public int getStartTime()  { return startTime; }
+    public int getEndTime()    { return endTime; }
+    public int getStartBurst() { return startBurst; }
+    public int getStopBurst()  { return stopBurst; }
 }
